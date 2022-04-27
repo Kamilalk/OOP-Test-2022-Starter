0279e2e87083d59c4d7eae4a1d6b628f5d2aa3db
KamilaLK <03KLKrukowska@gmail.com>
2022-04-27 16:55:52 +0100

    Nematode class filled with fields, a constructor, accessor m,athods and a toString method

--- java/src/ie/tudublin/NematodeVisualiser.java
+++ java/src/ie/tudublin/NematodeVisualiser.java
@@ -7,7 +7,90 @@ import processing.data.Table;
 import processing.data.TableRow;
 
 public class NematodeVisualiser extends PApplet
-{
+{	
+
+	private String name;
+	private float length;
+	private boolean limbs;
+	private String gender;
+	private boolean eyes;
+
+	@Override
+	public String toString() {
+		return "NematodeVisualiser [eyes=" + eyes + ", gender=" + gender + ", length=" + length + ", limbs=" + limbs
+				+ ", name=" + name + "]";
+	}
+
+	public NematodeVisualiser(TableRow tr)
+	{
+		this(
+			tr.getString("Display Name"),
+			tr.getFloat("Length"),
+			tr.getInt("Limbs") == 1,
+			tr.getString("Gender"),
+			tr.getInt("Eyes") == 1
+		);
+		
+	}
+
+	public NematodeVisualiser(String name, float length, boolean limbs, String gender, boolean eyes) {
+		this.name = name;
+		this.length = length;
+		this.limbs = limbs;
+		this.gender = gender;
+		this.eyes = eyes;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public float getLength() {
+		return length;
+	}
+
+
+	public void setLength(float length) {
+		this.length = length;
+	}
+
+
+	public boolean isLimbs() {
+		return limbs;
+	}
+
+
+	public void setLimbs(boolean limbs) {
+		this.limbs = limbs;
+	}
+
+
+	public String getGender() {
+		return gender;
+	}
+
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+
+	public boolean isEyes() {
+		return eyes;
+	}
+
+
+	public void setEyes(boolean eyes) {
+		this.eyes = eyes;
+	}
+
 
 	public void keyPressed()
 	{		
@@ -32,6 +115,7 @@ public class NematodeVisualiser extends PApplet
 
 	public void loadNematodes()
 	{
+
 	}
 
 
