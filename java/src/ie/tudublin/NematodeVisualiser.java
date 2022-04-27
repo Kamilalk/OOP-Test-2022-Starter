e9a46d2a6f09172e23e52dd8b979dbef95628f85
KamilaLK <03KLKrukowska@gmail.com>
2022-04-27 17:42:20 +0100

    cvs file loaded into array and displayed

--- java/src/ie/tudublin/NematodeVisualiser.java
+++ java/src/ie/tudublin/NematodeVisualiser.java
@@ -8,88 +8,7 @@ import processing.data.TableRow;
 
 public class NematodeVisualiser extends PApplet
 {	
-
-	private String name;
-	private float length;
-	private boolean limbs;
-	private String gender;
-	private boolean eyes;
-
-	@Override
-	public String toString() {
-		return "NematodeVisualiser [eyes=" + eyes + ", gender=" + gender + ", length=" + length + ", limbs=" + limbs
-				+ ", name=" + name + "]";
-	}
-
-	public NematodeVisualiser(TableRow tr)
-	{
-		this(
-			tr.getString("Display Name"),
-			tr.getFloat("Length"),
-			tr.getInt("Limbs") == 1,
-			tr.getString("Gender"),
-			tr.getInt("Eyes") == 1
-		);
-		
-	}
-
-	public NematodeVisualiser(String name, float length, boolean limbs, String gender, boolean eyes) {
-		this.name = name;
-		this.length = length;
-		this.limbs = limbs;
-		this.gender = gender;
-		this.eyes = eyes;
-	}
-
-
-	public String getName() {
-		return name;
-	}
-
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-
-	public float getLength() {
-		return length;
-	}
-
-
-	public void setLength(float length) {
-		this.length = length;
-	}
-
-
-	public boolean isLimbs() {
-		return limbs;
-	}
-
-
-	public void setLimbs(boolean limbs) {
-		this.limbs = limbs;
-	}
-
-
-	public String getGender() {
-		return gender;
-	}
-
-
-	public void setGender(String gender) {
-		this.gender = gender;
-	}
-
-
-	public boolean isEyes() {
-		return eyes;
-	}
-
-
-	public void setEyes(boolean eyes) {
-		this.eyes = eyes;
-	}
+	ArrayList<Nematode> nematodes = new ArrayList<Nematode>(); 
 
 
 	public void keyPressed()
@@ -109,12 +28,28 @@ public class NematodeVisualiser extends PApplet
 	{
 		colorMode(HSB);
 		background(0);
+		loadNematodes();
+		printNematodes();
 		smooth();				
+	}
+
+	void printNematodes()
+	{
+		for(Nematode n:nematodes)
+		{
+			System.out.println(n);
+		}
 	}
 	
 
 	public void loadNematodes()
 	{
+		Table table = loadTable("nematodes.csv", "header");
+		for(TableRow r:table.rows())
+		{
+			Nematode n = new Nematode(r);
+			nematodes.add(n);
+		}
 
 	}
 
